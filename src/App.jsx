4c6bc52045a4369b9d9f0cf5236ecf5c9c8a41f8
Ivaylo Milanov <imilanov24@gmail.com>
2025-02-23 10:56:08 +0200

    add React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
-    return <></>;
+import Routes from "./Routes";
+
+const App = () => {
+    return (
+        <BrowserRouter>
+           <Routes />
+        </BrowserRouter>
+    );
 }
 
 export default App;
