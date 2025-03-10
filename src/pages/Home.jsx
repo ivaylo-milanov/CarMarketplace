3ce9207c16667038cf1734be7d88a86254b54872
Ivaylo Milanov <imilanov24@gmail.com>
2025-03-10 19:53:57 +0200

    refactor Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className="relative  container mx-auto p-4 max-w-5xl mx-auto">
+            <div className="relative mx-auto p-4 max-w-5xl mx-auto">
                 <div className="mb-8 p-4 bg-gray-100 rounded-lg shadow-lg">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                         <div>
